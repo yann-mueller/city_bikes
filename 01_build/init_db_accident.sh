5606fa9c2abb55c82b2b7a7969dc931bb9b01b74
Yann Müller <yann.mueller.work@gmail.com>
2025-04-24 18:27:58 +0200

    Create Seperate Files for Database Creation (CitiBike Rides and NYPD Accidents)

---
+++ 01_build/init_db_accident.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+DB_NAME="nypd"
+DB_USER="postgres"
+DB_PASSWORD="axa_datascience"
+DB_HOST="localhost"
+DB_PORT="5432"
+
+# Create the database if it doesn't exist
+PGPASSWORD=$DB_PASSWORD psql -h $DB_HOST -U $DB_USER -p $DB_PORT -tc "SELECT 1 FROM pg_database WHERE datname = '$DB_NAME'" | grep -q 1 || \
+PGPASSWORD=$DB_PASSWORD psql -h $DB_HOST -U $DB_USER -p $DB_PORT -c "CREATE DATABASE $DB_NAME"
+
+echo "Database '$DB_NAME' created (or already exists)."
